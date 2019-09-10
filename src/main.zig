cbe54abd26e02bc5b6c47afc4cec53d75142030d
Andre Weissflog <floooh@gmail.com>
2019-09-10 19:29:28 +0200

    a bit more sokol_gfx.zig work

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const sapp = @import("sokol/sokol_app.zig").SokolApp;
 const sg = @import("sokol/sokol_gfx.zig").SokolGfx;
+const warn = @import("std").debug.warn;
 
 var pass_action: sg.PassAction = undefined;
 
@@ -14,10 +15,36 @@ fn init_cb() void {
             sg.ColorAttachmentAction { .action = .SG_ACTION_CLEAR, .val = [_]f32 {1.0, 0.0, 0.0} }
         }
     };
+
+    var bind = sg.Bindings { };
+    var buf = sg.Buffer { .id=123 };
+    var img = sg.Image { .id=234 };
+
+    warn("Backend: ");
+    switch (sg.query_backend()) {
+        .SG_BACKEND_GLCORE33 => { warn("GLCORE33\n"); },
+        .SG_BACKEND_GLES2 => { warn("GLES2\n"); },
+        .SG_BACKEND_GLES3 => { warn("GLES3\n"); },
+        .SG_BACKEND_D3D11 => { warn("D3D11\n"); },
+        .SG_BACKEND_METAL_IOS => { warn("METAL_IOS\n"); },
+        .SG_BACKEND_METAL_MACOS => { warn("METAL_MACOS\n"); },
+        .SG_BACKEND_METAL_SIMULATOR => { warn("METAL_SIMULATOR\n"); },
+        else => { warn("???"); }
+    }
+
+    warn("Features:\n");
+    const features = sg.query_features();
+    warn("  instancing: {}\n", features.instancing);
+    warn("  origin_top_left: {}\n", features.origin_top_left);
+    warn("  multiple_render_targets: {}\n", features.multiple_render_targets);
+    warn("  msaa_render_targets: {}\n", features.msaa_render_targets);
+    warn("  imagetype_3d: {}\n", features.imagetype_3d);
+    warn("  imagetype_array: {}\n", features.imagetype_array);
+    warn("  image_clamp_to_border: {}\n", features.image_clamp_to_border);
 }
 
 fn frame_cb() void {
-    var g = pass_action.colors[0].val[1] + 0.01;
+    const g = pass_action.colors[0].val[1] + 0.01;
     pass_action.colors[0].val[1] = if (g > 1.0) 0.0 else g;
     sg.begin_default_pass(pass_action, sapp.width(), sapp.height());
     sg.end_pass();
--- src/sokol/sokol_gfx.zig
+++ src/sokol/sokol_gfx.zig
@@ -7,56 +7,95 @@ const c = @cImport({
 
 pub const SokolGfx = struct {
 
-pub const Desc = struct {
-    _start_canary: u32 = 0,
-    buffer_pool_size: c_int = 0,
-    image_pool_size: c_int = 0,
-    shader_pool_size: c_int = 0,
-    pipeline_pool_size: c_int = 0,
-    pass_pool_size: c_int = 0,
-    context_pool_size: c_int = 0,
-    gl_force_gles2: bool = false,
-    mtl_device: ?*const c_void = null,
-    mtl_renderpass_descriptor_cb: ?extern fn() ?*const c_void = null,
-    mtl_drawable_cb: ?extern fn() ?*const c_void = null,
-    mtl_global_uniform_buffer_size: c_int = 0,
-    mtl_sampler_cache_size: c_int = 0,
-    d3d11_device: ?*const c_void = null,
-    d3d11_device_context: ?*const c_void = null,
-    d3d11_render_target_view_cb: ?extern fn() ?*const c_void = null,
-    d3d11_depth_stencil_view_cb: ?extern fn() ?*const c_void = null,
-    _end_canary: u32 = 0
-};
+pub const MaxShaderStageBuffers = c.SG_MAX_SHADERSTAGE_BUFFERS;
+pub const MaxShaderStageImages = c.SG_MAX_SHADERSTAGE_IMAGES;
+pub const MaxColorAttachments = c.SG_MAX_COLOR_ATTACHMENTS;
 
+pub const Buffer = struct { id:u32 = 0 };
+pub const Image = struct { id:u32 = 0 };
+pub const Shader = struct { id:u32 = 0 };
+pub const Pipeline = struct { id:u32 = 0 };
+pub const Pass = struct { id:u32 = 0 };
+
+pub const Backend = c.sg_backend;
+pub const PixelFormat = c.sg_pixel_format;
+pub const PixelFormatInfo = c.sg_pixelformat_info;
+pub const Features = c.sg_features;
+pub const Limits = c.sg_limits;
+pub const ResourceState = c.sg_resource_state;
+pub const Usage = c.sg_usage;
+pub const BufferType = c.sg_buffer_type;
+pub const IndexType = c.sg_index_type;
+pub const ImageType = c.sg_image_type;
+pub const CubeFace = c.sg_cube_face;
+pub const ShaderStage = c.sg_shader_stage;
+pub const PrimitiveType = c.sg_primitive_type;
+pub const Filter = c.sg_filter;
+pub const Wrap = c.sg_wrap;
+pub const BorderColor = c.sg_border_color;
+pub const VertexFormat = c.sg_vertex_format;
+pub const VertexStep = c.sg_vertex_step;
+pub const UniformType = c.sg_uniform_type;
+pub const CullMode = c.sg_cull_mode;
+pub const FaceWinding = c.sg_face_winding;
+pub const CompareFunc = c.sg_compare_func;
+pub const StencilOp = c.sg_stencil_op;
+pub const BlendFactor = c.sg_blend_factor;
+pub const BlendOp = c.sg_blend_op;
+pub const ColorMask = c.sg_color_mask;
 pub const Action = c.sg_action;
+
 pub const ColorAttachmentAction = struct {
-    action: Action = Action._SG_ACTION_DEFAULT,
+    action: Action = ._SG_ACTION_DEFAULT,
     val: [4]f32 = [4]f32{ 0, 0, 0, 1 }
 };
+
 pub const DepthAttachmentAction = struct {
-    action: Action = Action._SG_ACTION_DEFAULT,
+    action: Action = ._SG_ACTION_DEFAULT,
     val: f32 = 0.0
 };
+
 pub const StencilAttachmentAction = struct {
-    action: Action = Action._SG_ACTION_DEFAULT,
+    action: Action = ._SG_ACTION_DEFAULT,
     val: u8 = 0,
 };
 
 pub const PassAction = struct {
-    _start_canary: u32 = 0,
-    colors: [4]ColorAttachmentAction = [4].ColorAttachmentAction {
-        ColorAttachmentAction { },
-        ColorAttachmentAction { },
-        ColorAttachmentAction { },
-        ColorAttachmentAction { },
-    },
+    colors: [MaxColorAttachments]ColorAttachmentAction = [_].ColorAttachmentAction {ColorAttachmentAction{}} ** MaxColorAttachments,
     depth: DepthAttachmentAction = DepthAttachmentAction { },
     stencil: StencilAttachmentAction = StencilAttachmentAction { },
-    _end_canary: u32 = 0,
 };
 
-pub fn setup(desc: Desc) void {
-    const sg_desc = c.sg_desc {
+pub const Bindings = struct {
+    vertex_buffers: [MaxShaderStageBuffers]Buffer = [_]Buffer {Buffer{}} ** MaxShaderStageBuffers,
+    vertex_buffer_offsets: [MaxShaderStageBuffers]i32 = [_]i32 {0} ** MaxShaderStageBuffers,
+    index_buffer: Buffer = Buffer{},
+    index_buffer_offset: i32 = 0,
+    vs_images: [MaxShaderStageImages]Image = [_]Image {Image{}} ** MaxShaderStageImages,
+    fs_images: [MaxShaderStageImages]Image = [_]Image {Image{}} ** MaxShaderStageImages,
+};
+
+pub const Desc = struct {
+    buffer_pool_size: i32 = 0,
+    image_pool_size: i32 = 0,
+    shader_pool_size: i32 = 0,
+    pipeline_pool_size: i32 = 0,
+    pass_pool_size: i32 = 0,
+    context_pool_size: i32 = 0,
+    gl_force_gles2: bool = false,
+    mtl_device: ?*const c_void = null,
+    mtl_renderpass_descriptor_cb: ?extern fn() ?*const c_void = null,
+    mtl_drawable_cb: ?extern fn() ?*const c_void = null,
+    mtl_global_uniform_buffer_size: i32 = 0,
+    mtl_sampler_cache_size: i32 = 0,
+    d3d11_device: ?*const c_void = null,
+    d3d11_device_context: ?*const c_void = null,
+    d3d11_render_target_view_cb: ?extern fn() ?*const c_void = null,
+    d3d11_depth_stencil_view_cb: ?extern fn() ?*const c_void = null,
+};
+
+fn conv_desc(desc:Desc) c.sg_desc {
+    return c.sg_desc {
         ._start_canary = 0,
         .buffer_pool_size = desc.buffer_pool_size,
         .image_pool_size = desc.image_pool_size,
@@ -76,16 +115,12 @@ pub fn setup(desc: Desc) void {
         .d3d11_depth_stencil_view_cb = desc.d3d11_depth_stencil_view_cb,
         ._end_canary = 0
     };
-    c.sg_setup(&sg_desc);
-}
-pub fn shutdown() void {
-    c.sg_shutdown();
 }
 
-pub fn begin_default_pass(pass_action: PassAction, width: c_int, height: c_int) void {
-    const ps = c.sg_pass_action{
+fn conv_pass_action(pass_action: PassAction) c.sg_pass_action {
+    return c.sg_pass_action{
         ._start_canary = 0,
-        .colors = [4]c.sg_color_attachment_action {
+        .colors = [_]c.sg_color_attachment_action {
             c.sg_color_attachment_action {
                 .action = pass_action.colors[0].action,
                 .val = pass_action.colors[0].val,
@@ -113,14 +148,72 @@ pub fn begin_default_pass(pass_action: PassAction, width: c_int, height: c_int) 
         },
         ._end_canary = 0,
     };
-    c.sg_begin_default_pass(&ps, width, height);
 }
+
+//== PUBLIC FUNCTIONS ==========================================================
+pub fn setup(desc: Desc) void {
+    c.sg_setup(&conv_desc(desc));
+}
+
+pub fn shutdown() void {
+    c.sg_shutdown();
+}
+
+pub fn isvalid() bool {
+    return c.sg_isvalid();
+}
+
+pub fn reset_state_cache() void {
+    c.sg_reset_state_cache();
+}
+
+// FIXME: install_trace_hooks
+// FIXME: push_debug_group
+// FIMXE: pop_debug_group
+
+// FIXME: make_buffer
+// FIXME: make_image
+// FIXME: make_pipeline
+// FIXME: make_pass
+// FIXME: destroy_buffer
+// FIXME: destroy_image
+// FIXME: destroy_shader
+// FIXME: destroy_pipeline
+// FIXME: destroy_pass
+// FIXME: update_buffer
+// FIXME: update_image
+// FIXME: append_buffer
+// FIXME: query_buffer_overflow
+
+pub fn begin_default_pass(pass_action: PassAction, width: c_int, height: c_int) void {
+    c.sg_begin_default_pass(&conv_pass_action(pass_action), width, height);
+}
+
+// FIXME: begin_pass
+// FIXME: apply_viewport
+// FIXME: apply_scissor_rect
+// FIXME: apply_pipeline
+// FIXME: apply_bindings
+// FIXME: apply_uniforms
+// FIXME: draw
+
 pub fn end_pass() void {
     c.sg_end_pass();
 }
 
 pub fn commit() void {
     c.sg_commit();
+}
+
+// FIXME: query_desc
+
+pub fn query_backend() Backend {
+    return c.sg_query_backend();
+}
+
+pub fn query_features() Features {
+    // FIXME FIXME FIXME: returned struct has random values
+    return c.sg_query_features();
 }
 
 }; // sokol-gfx
