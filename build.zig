25952fd354095091e017d7e550ca2a8aca1c8477
Andre Weissflog <floooh@gmail.com>
2020-08-15 17:35:11 +0200

    build.zig: don't actually need to builtin module

--- build.zig
+++ build.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const Builder = std.build.Builder;
-const builtin = @import("builtin");
 
 pub fn target(b: *Builder, comptime name: []const u8, source: []const u8) void {
     const mode = b.standardReleaseOptions();
