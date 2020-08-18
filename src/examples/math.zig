0fd2d475054910b0cf223cccbe6f9745900e287c
Andre Weissflog <floooh@gmail.com>
2020-08-18 18:28:51 +0200

    math helper script complete

--- src/examples/math.zig
+++ src/examples/math.zig
@@ -3,9 +3,15 @@
 //
 //  minimal vector math helper functions, just the stuff needed for
 //  the sokol-samples
+//
+//  Ported from HandmadeMath.h
 //------------------------------------------------------------------------------
 const assert = @import("std").debug.assert;
 const math = @import("std").math;
+
+fn radians(deg: f32) f32 {
+    return deg * (math.pi / 180.0);
+}
 
 pub const Vec3 = packed struct {
     x: f32, y: f32, z: f32,
@@ -16,6 +22,48 @@ pub const Vec3 = packed struct {
 
     pub fn new(x: f32, y: f32, z: f32) Vec3 {
         return Vec3 { .x=x, .y=y, .z=z };
+    }
+
+    pub fn up() Vec3 {
+        return Vec3 { .x=0.0, .y=1.0, .z=0.0 };
+    }
+
+    pub fn len(v: Vec3) f32 {
+        return math.sqrt(Vec3.dot(v, v));
+    }
+
+    pub fn sub(left: Vec3, right: Vec3) Vec3 {
+        return Vec3 {
+            .x = left.x - right.x,
+            .y = left.y - right.y,
+            .z = left.z - right.z
+        };
+    }
+
+    pub fn norm(v: Vec3) Vec3 {
+        const l = Vec3.len(v);
+        if (l != 0.0) {
+            return Vec3 {
+                .x = v.x / l,
+                .y = v.y / l,
+                .z = v.z / l
+            };
+        }
+        else {
+            return Vec3.zero();
+        }
+    }
+
+    pub fn cross(v0: Vec3, v1: Vec3) Vec3 {
+        return Vec3 {
+            .x = (v0.y * v1.z) - (v0.z * v1.y),
+            .y = (v0.z * v1.x) - (v0.x * v1.z),
+            .z = (v0.x * v1.y) - (v0.y * v1.x)
+        };
+    }
+
+    pub fn dot(v0: Vec3, v1: Vec3) f32 {
+        return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
     }
 };
 
@@ -70,6 +118,54 @@ pub const Mat4 = packed struct {
         res.m[3][3] = 0.0;
         return res;
     }
+
+    pub fn lookat(eye: Vec3, center: Vec3, up: Vec3) Mat4 {
+        var res = Mat4.zero();
+
+        const f = Vec3.norm(Vec3.sub(center, eye));
+        const s = Vec3.norm(Vec3.cross(f, up));
+        const u = Vec3.cross(s, f);
+
+        res.m[0][0] = s.x;
+        res.m[0][1] = u.x;
+        res.m[0][2] = -f.x;
+
+        res.m[1][0] = s.y;
+        res.m[1][1] = u.y;
+        res.m[1][2] = -f.y;
+
+        res.m[2][0] = s.z;
+        res.m[2][1] = u.z;
+        res.m[2][2] = -f.z;
+
+        res.m[3][0] = -Vec3.dot(s, eye);
+        res.m[3][1] = -Vec3.dot(u, eye);
+        res.m[3][2] = Vec3.dot(f, eye);
+        res.m[3][3] = 1.0;
+
+        return res;
+    }
+
+    pub fn rotate(angle: f32, axis_unorm: Vec3) Mat4 {
+        var res = Mat4.identity();
+
+        const axis = Vec3.norm(axis_unorm);
+        const sin_theta = math.sin(radians(angle));
+        const cos_theta = math.cos(radians(angle));
+        const cos_value = 1.0 - cos_theta;
+
+        res.m[0][0] = (axis.x * axis.x * cos_value) + cos_theta;
+        res.m[0][1] = (axis.x * axis.y * cos_value) + (axis.z * sin_theta);
+        res.m[0][2] = (axis.x * axis.z * cos_value) - (axis.y * sin_theta);
+        res.m[1][0] = (axis.y * axis.x * cos_value) - (axis.z * sin_theta);
+        res.m[1][1] = (axis.y * axis.y * cos_value) + cos_theta;
+        res.m[1][2] = (axis.y * axis.z * cos_value) + (axis.x * sin_theta);
+        res.m[2][0] = (axis.z * axis.x * cos_value) + (axis.y * sin_theta);
+        res.m[2][1] = (axis.z * axis.y * cos_value) - (axis.x * sin_theta);
+        res.m[2][2] = (axis.z * axis.z * cos_value) + cos_theta;
+
+        return res;
+    }
 };
 
 test "Vec3.zero" {
@@ -112,34 +208,82 @@ test "Mat4.mul"{
     }
 }
 
-fn eq(val: f32, cmp: f32, delta: f32) bool {
+fn eq(val: f32, cmp: f32) bool {
+    const delta: f32 = 0.00001;
     return (val > (cmp-delta)) and (val < (cmp+delta));
 }
 
 test "Mat4.persp" {
     const m = Mat4.persp(60.0, 1.33333337, 0.01, 10.0);
 
-    assert(eq(m.m[0][0], 1.73205, 0.00001));
-    assert(m.m[0][1] == 0.0);
-    assert(m.m[0][2] == 0.0);
-    assert(m.m[0][3] == 0.0);
+    assert(eq(m.m[0][0], 1.73205));
+    assert(eq(m.m[0][1], 0.0));
+    assert(eq(m.m[0][2], 0.0));
+    assert(eq(m.m[0][3], 0.0));
 
-    assert(m.m[1][0] == 0.0);
-    assert(eq(m.m[1][1], 2.30940, 0.00001));
-    assert(m.m[1][2] == 0.0);
-    assert(m.m[1][3] == 0.0);
+    assert(eq(m.m[1][0], 0.0));
+    assert(eq(m.m[1][1], 2.30940));
+    assert(eq(m.m[1][2], 0.0));
+    assert(eq(m.m[1][3], 0.0));
 
-    assert(m.m[2][0] == 0.0);
-    assert(m.m[2][1] == 0.0);
-    assert(eq(m.m[2][2], -1.00200, 0.00001));
-    assert(m.m[2][3] == -1.0);
+    assert(eq(m.m[2][0], 0.0));
+    assert(eq(m.m[2][1], 0.0));
+    assert(eq(m.m[2][2], -1.00200));
+    assert(eq(m.m[2][3], -1.0));
 
-    assert(m.m[3][0] == 0.0);
-    assert(m.m[3][1] == 0.0);
-    assert(eq(m.m[3][2], -0.02002, 0.00001));
-    assert(m.m[3][3] == 0.0);
+    assert(eq(m.m[3][0], 0.0));
+    assert(eq(m.m[3][1], 0.0));
+    assert(eq(m.m[3][2], -0.02002));
+    assert(eq(m.m[3][3], 0.0));
 }
 
+test "Mat4.lookat" {
+    const m = Mat4.lookat(.{ .x=0.0, .y=1.5, .z=6.0 }, Vec3.zero(), Vec3.up());
+
+    assert(eq(m.m[0][0], 1.0));
+    assert(eq(m.m[0][1], 0.0));
+    assert(eq(m.m[0][2], 0.0));
+    assert(eq(m.m[0][3], 0.0));
+
+    assert(eq(m.m[1][0], 0.0));
+    assert(eq(m.m[1][1], 0.97014));
+    assert(eq(m.m[1][2], 0.24253));
+    assert(eq(m.m[1][3], 0.0));
+
+    assert(eq(m.m[2][0], 0.0));
+    assert(eq(m.m[2][1], -0.24253));
+    assert(eq(m.m[2][2], 0.97014));
+    assert(eq(m.m[2][3], 0.0));
+
+    assert(eq(m.m[3][0], 0.0));
+    assert(eq(m.m[3][1], 0.0));
+    assert(eq(m.m[3][2], -6.18465));
+    assert(eq(m.m[3][3], 1.0));
+}
+
+test "Mat4.rotate" {
+    const m = Mat4.rotate(2.0, .{ .x=0.0, .y=1.0, .z=0.0 });
+
+    assert(eq(m.m[0][0], 0.99939));
+    assert(eq(m.m[0][1], 0.0));
+    assert(eq(m.m[0][2], -0.03489));
+    assert(eq(m.m[0][3], 0.0));
+
+    assert(eq(m.m[1][0], 0.0));
+    assert(eq(m.m[1][1], 1.0));
+    assert(eq(m.m[1][2], 0.0));
+    assert(eq(m.m[1][3], 0.0));
+
+    assert(eq(m.m[2][0], 0.03489));
+    assert(eq(m.m[2][1], 0.0));
+    assert(eq(m.m[2][2], 0.99939));
+    assert(eq(m.m[2][3], 0.0));
+
+    assert(eq(m.m[3][0], 0.0));
+    assert(eq(m.m[3][1], 0.0));
+    assert(eq(m.m[3][2], 0.0));
+    assert(eq(m.m[3][3], 1.0));
+}
 
 
 
