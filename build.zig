74eacf07157fbd8a4c3a1ad482475c011aef84a3
Andre Weissflog <floooh@gmail.com>
2021-01-02 19:37:11 +0100

    some more minor build script cleanup

--- build.zig
+++ build.zig
@@ -1,15 +1,10 @@
-const Builder = @import("std").build.Builder;
-const LibExeObjStep = @import("std").build.LibExeObjStep;
-const builtin = @import("std").builtin;
+const bld = @import("std").build;
 const mem = @import("std").mem;
+const zig = @import("std").zig;
 
 // macOS helper function to add SDK search paths
-fn macosAddSdkDirs(b: *Builder, step: *LibExeObjStep) !void {
-    var sdk_dir = try b.exec(&[_][]const u8 { "xcrun", "--show-sdk-path" });
-    const newline_index = mem.lastIndexOf(u8, sdk_dir, "\n");
-    if (newline_index) |idx| {
-        sdk_dir = sdk_dir[0..idx];
-    }
+fn macosAddSdkDirs(b: *bld.Builder, step: *bld.LibExeObjStep) !void {
+    const sdk_dir = try zig.system.getSDKPath(b.allocator);
     const framework_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/System/Library/Frameworks" });
     const usrinclude_dir = try mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/usr/include"});
     step.addFrameworkDir(framework_dir);
@@ -17,10 +12,11 @@ fn macosAddSdkDirs(b: *Builder, step: *LibExeObjStep) !void {
 }
 
 // build sokol into a static library
-fn buildSokol(b: *Builder) *LibExeObjStep {
+fn buildSokol(b: *bld.Builder) *bld.LibExeObjStep {
     const lib = b.addStaticLibrary("sokol", null);
+    lib.linkLibC();
     lib.setBuildMode(b.standardReleaseOptions());
-    if (builtin.os.tag == .macos) {
+    if (lib.target.isDarwin()) {
         macosAddSdkDirs(b, lib) catch unreachable;
         lib.addCSourceFile("src/sokol/sokol.c", &[_][]const u8 { "-ObjC" });
         lib.linkFramework("MetalKit");
@@ -31,8 +27,7 @@ fn buildSokol(b: *Builder) *LibExeObjStep {
     }
     else {
         lib.addCSourceFile("src/sokol/sokol.c", &[_][]const u8{});
-        lib.linkSystemLibrary("c");
-        if (builtin.os.tag == .linux) {
+        if (lib.target.isLinux()) {
             lib.linkSystemLibrary("X11");
             lib.linkSystemLibrary("Xi");
             lib.linkSystemLibrary("Xcursor");
@@ -44,7 +39,7 @@ fn buildSokol(b: *Builder) *LibExeObjStep {
 }
 
 // build one of the example exes
-fn buildExample(b: *Builder, sokol: *LibExeObjStep, comptime name: []const u8) void {
+fn buildExample(b: *bld.Builder, sokol: *bld.LibExeObjStep, comptime name: []const u8) void {
     const e = b.addExecutable(name, "src/examples/" ++ name ++ ".zig");
     e.linkLibrary(sokol);
     e.setBuildMode(b.standardReleaseOptions());
@@ -53,7 +48,7 @@ fn buildExample(b: *Builder, sokol: *LibExeObjStep, comptime name: []const u8) v
     b.step("run-" ++ name, "Run " ++ name).dependOn(&e.run().step);
 }
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *bld.Builder) void {
     const sokol = buildSokol(b);
     buildExample(b, sokol, "clear");
     buildExample(b, sokol, "triangle");
