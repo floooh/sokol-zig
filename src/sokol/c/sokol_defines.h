6b06efed461c8f6a6e69abdc97c7081b77424b6e
Andre Weissflog <floooh@gmail.com>
2023-02-14 18:59:45 +0100

    remove the SOKOL_LOG macro from sokol_defines.h, no longer needed

--- src/sokol/c/sokol_defines.h
+++ src/sokol/c/sokol_defines.h
@@ -2,7 +2,6 @@
 #define SOKOL_NO_ENTRY
 #if defined(_WIN32)
     #define SOKOL_WIN32_FORCE_MAIN
-    #define SOKOL_LOG(msg) OutputDebugStringA(msg)
 #endif
 // FIXME: macOS Zig HACK without this, some C stdlib headers throw errors
 #if defined(__APPLE__)
