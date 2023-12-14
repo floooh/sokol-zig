e1e1eb19d0a2e4756b69cc9578a0aee9deb5597e
Andrew Kachnic <ajkachnic@protonmail.com>
2023-12-14 15:18:54 -0500

    add missing dependencies

--- src/sokol/c/sokol_fontstash.c
+++ src/sokol/c/sokol_fontstash.c
@@ -2,10 +2,13 @@
 #define SOKOL_FONTSTASH_IMPL
 #define FONTSTASH_IMPLEMENTATION
 #endif
-#include "sokol_defines.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "sokol_defines.h"
 #include "sokol_gfx.h"
+#include "sokol_gl.h"
 #include "fontstash.h"
 #include "sokol_fontstash.h"
