104ea49316cc65c4b6f11f8f58128adf534e2b89
Andrew Kachnic <ajkachnic@protonmail.com>
2023-12-14 15:17:02 -0500

    hopefully fix building

--- src/sokol/c/sokol_fontstash.c
+++ src/sokol/c/sokol_fontstash.c
@@ -3,6 +3,9 @@
 #define FONTSTASH_IMPLEMENTATION
 #endif
 #include "sokol_defines.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "sokol_gfx.h"
 #include "fontstash.h"
 #include "sokol_fontstash.h"
