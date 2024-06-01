6859cc07f9f552d14cd1034a0bccd5f809f134e3
Andre Weissflog <floooh@gmail.com>
2024-06-01 15:43:16 +0200

    sokol_imgui.c: don't include cimgui.h when generating bindings

--- src/sokol/c/sokol_imgui.c
+++ src/sokol/c/sokol_imgui.c
@@ -1,9 +1,9 @@
 #if defined(IMPL)
 #define SOKOL_IMGUI_IMPL
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
 #endif
 #include "sokol_defines.h"
 #include "sokol_app.h"
 #include "sokol_gfx.h"
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
-#include "cimgui.h"
 #include "sokol_imgui.h"
