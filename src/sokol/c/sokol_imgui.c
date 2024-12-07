36b836caeed96d11b26c6593bc22634454b98a20
Andre Weissflog <floooh@gmail.com>
2024-12-07 17:03:49 +0100

    add a FIXME comment to sokol_imgui.c

--- src/sokol/c/sokol_imgui.c
+++ src/sokol/c/sokol_imgui.c
@@ -1,5 +1,6 @@
 #if defined(IMPL)
 #define SOKOL_IMGUI_IMPL
+// FIXME: no longer needed with dear_bindings, remove after transition period
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 #endif
